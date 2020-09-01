89ea712fa0170adc1f3fcec5a87af8d891252b60
ITWebwangxin <2677855779@qq.com>
2020-09-01 16:31:09 +0800

    修改提交

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,88 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表
+    list: [],
+    // 这是文本框的内容
+    imputValue: 'aaa',
+    // 下一个id
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    // 为store中的imputValue赋值
+    setInputValue(state, val) {
+      state.imputValue = val
+    },
+    // 添加列表项
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.imputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.imputValue = ''
+    },
+    // 根据id删除对应的任务事项
+    removeItem(state, id) {
+      // 根据id查找对应项的索引
+      const i = state.list.findIndex(item => item.id === id)
+      // 根据索引删除对应的元素
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    // 修改列表项的选中状态
+    changeStatus(state, param) {
+      const i = state.list.findIndex(item => item.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成
+    cleanDone(state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    // 修改视图的关键字
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        console.log(data)
+        context.commit('initList', data)
+      })
+    }
   },
-  modules: {
-  }
+  getters: {
+    // 统计未完成的任务的条数
+    unDoneLength(state) {
+      return state.list.filter(item => item.done === false).length
+    },
+    infolist(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(item => item.done === false)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(item => item.done === true)
+      }
+      return state.list
+    }
+  },
+  modules: {}
 })
